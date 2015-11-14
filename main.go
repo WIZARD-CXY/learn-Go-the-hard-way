662586fdcf2dc5d6b3e91e4a453669d596039eb5
wizard <wizard_cxy@hotmail.com>
2015-11-14 11:00:13 +0800

    finish l4

--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
 	"reflect"
 )
 
 func MakeMap(fpt interface{}) {
 	fnV := reflect.ValueOf(fpt).Elem()
-	fmt.Println(fnV.Type().NumOut())
+	//fmt.Println(fnV.Type().NumOut())
 	fnI := reflect.MakeFunc(fnV.Type(), implMap)
-	fmt.Println("haha1", fnI.Type().NumOut())
+	//fmt.Println("haha1", fnI.Type().NumOut())
 	fnV.Set(fnI)
 }
 
 //TODO:completes implMap function.
+// note the declarition
+// func MakeFunc(typ Type, fn func(args []Value) (results []Value)) Value
 var implMap func(in []reflect.Value) []reflect.Value = func(in []reflect.Value) (res []reflect.Value) {
-	fmt.Println(in)
-	fmt.Println(in[0].Type(), in[1].Len())
 	//in[0] is func, in[1] is slice or map
+	//fmt.Println(in[0].Type(), in[1].Type())
 
-	for i := 0; i < in[1].Len(); i++ {
-		inslice := make([]reflect.Value, 1)
-		inslice[0] = in[1].Index(i)
-		res = append(res, in[0].Call(inslice)[0])
+	if in[1].Kind() == reflect.Slice {
+		for i := 0; i < in[1].Len(); i++ {
+			inslice := make([]reflect.Value, 1)
+			inslice[0] = in[1].Index(i)
+			in[1].Index(i).Set(in[0].Call(inslice)[0])
+		}
+
+	} else {
+		//in[1] is map
+		for _, k := range in[1].MapKeys() {
+			inslice := make([]reflect.Value, 1)
+			inslice[0] = in[1].MapIndex(k)
+			in[1].SetMapIndex(k, in[0].Call(inslice)[0])
+		}
+
 	}
+	// The result Value slice returned by fn (res here)must have the number and type of results given by typ(fnV.Type() here).
+	// In this case f is func(func(e int) int, []int) []int, so len(res) must be 1
+	// if f2 is func(func(e int) int, []int) ([]int, []int) then len(res) should be 2
+	res = make([]reflect.Value, 1)
+	res[0] = in[1]
 
-	fmt.Println("len", len(res))
 	return
 }
 
