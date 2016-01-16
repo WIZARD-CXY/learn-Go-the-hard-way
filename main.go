1aebf9a4ba965f0c693accefea3e4b25fcb8865d
wizard <wizard_cxy@hotmail.com>
2016-01-16 15:36:41 +0800

    finish l6

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -17,17 +18,23 @@ type route struct {
 	r           string        //route url
 	method      string        //method (GET)
 	httpHandler http.Handler  //custome handler is allowed.
-	handler     reflect.Value //handle func
+	handler     reflect.Value //handler func
 }
 
-//implements http.Handle
+//implements http.Handler
 func (s *Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	for _, r := range s.routes {
 		if r.r == req.URL.Path {
 			if r.httpHandler != nil {
 				r.httpHandler.ServeHTTP(res, req)
 			} else {
-				//TODO:pass the contex to the function and write return value to res.
+				//TODO:pass the context to the function and write return value to res.
+				ctx := &Context{req, res, s, make(map[string]string)}
+				in := make([]reflect.Value, 1)
+				in[0] = reflect.ValueOf(ctx)
+				resp := r.handler.Call(in)[0]
+				fmt.Fprintf(res, "%s", resp)
+
 			}
 		}
 	}
@@ -44,7 +51,7 @@ func (s *Server) Close() {
 func (s *Server) Run() {
 	mux := http.NewServeMux()
 	mux.Handle("/", s)
-	log.Printf("start serverving...\nPlease visit http://localhost:3000")
+	log.Printf("start servering...\nPlease visit http://localhost:3000")
 	l, err := net.Listen("tcp", s.addr)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
@@ -58,11 +65,14 @@ func (s *Server) Run() {
 func (s *Server) Get(rt string, handler interface{}) {
 	switch handler.(type) {
 	case http.Handler:
+		fmt.Println("handler")
 		s.routes = append(s.routes, route{r: rt, method: "GET", httpHandler: handler.(http.Handler)})
 	case reflect.Value:
+		fmt.Println("reflect.Value")
 		fv := handler.(reflect.Value)
 		s.routes = append(s.routes, route{r: rt, method: "GET", handler: fv})
 	default:
+		fmt.Println("default")
 		fv := reflect.ValueOf(handler)
 		s.routes = append(s.routes, route{r: rt, method: "GET", handler: fv})
 	}
@@ -87,10 +97,10 @@ func init() {
 }
 
 func main() {
-	println(`Go's field is backend.In this exercise,we focus on web framework,it shortens our development time and reduces our coding work.
+	println(`Go's field is backend. In this exercise, we focus on web framework, it shortens our development time and reduces our coding work.
 We will implement a function handler based tiny webframework.
 The first part is context management.Next part will focus on middleware.
 Now edit main.go file to Complete 'Server.ServeHttp()'.
 In this method you need to call the handler in the context and pass context as paramater if in the signature,
-and also write the return valut to the responseWriter. `)
+and also write the return value to the responseWriter. `)
 }
