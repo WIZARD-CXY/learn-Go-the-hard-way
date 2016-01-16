23c3d52595387d48b1a52114a7f107241a9cc611
wizard <wizard_cxy@hotmail.com>
2016-01-16 21:09:56 +0800

    finish l9

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ type Cheat struct {
 
 //TODO:complete the struct to be encoded by encoding/json.
 type Content struct {
+	Comment string
+	Command string
 }
 
 func CheatSheet(command string) string {
@@ -27,16 +29,28 @@ func CheatSheet(command string) string {
 	//TODO:find the name of which cheatsheet matchs command
 	//and add to out.
 	// ...
+	for _, item := range cheats {
+		if item.Name == command {
+			for _, output := range item.Contents {
+				out += output.Comment
+				out += "\n"
+				out += output.Command
+			}
+		}
+
+		break
+	}
+	out += "\n"
 	return out
 }
 
 func main() {
 	args := os.Args
 	if len(args) != 1 {
-		log.Fatal("want one  argument")
+		log.Fatal("want one argument")
 	}
 
-	println(`Unix has a lot of commands to remenber.
-To help us search the command quickly,we will create a small cheat sheet command.
+	println(`Unix has a lot of commands to remember.
+To help us search the command quickly,we will create a small cheatsheet command.
 We will store the commands as json.In this exercise you can play with Go IO and json encoding.`)
 }
